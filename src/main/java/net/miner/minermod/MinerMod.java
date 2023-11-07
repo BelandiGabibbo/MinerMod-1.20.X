e40239cf63da5d3310b24c288140e102becb05f6
Miner <damianopiscedda14@gmail.com>
2023-11-07 10:32:21 +0100

    Very important change

---
+++ src/main/java/net/miner/minermod/MinerMod.java
@@ -0,0 +1,17 @@
+package net.miner.minermod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MinerMod implements ModInitializer {
+	public static final String MOD_ID = "minermod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/net/miner/minermod/MinerModClient.java
@@ -0,0 +1,10 @@
+package net.miner.minermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinerModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
